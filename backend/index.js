0e3040d6995c866766f56097e4c5f807fbfa27c6
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2020-01-14 21:36:44 +0000

    Instalação de mongoose e teste de rotas

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    return res.json({version: "1.0.0"})
+mongoose.connect('mongodb+srv://leanfj:leanfj@cluster0-ipytm.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+app.use(express.json())
+
+//Query params: req.query - (Filtros, ordenação, paginação)
+//Route params: req.params - (Identificar um recurso para alteração ou remoção)
+//Body: req.body - ()
+
+app.get('/', (req, res, next) => {
+    console.log(req.query);
+    return res.json({version: "1.0.0"});
+});
+
+
+app.delete('/user/:id', (req, res, next) => {
+    console.log(req.params);
+});
+
+app.post('/user', (req, res, next) => {
+    console.log(req.body);
+})
 
 app.listen(3333);
