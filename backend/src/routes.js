7124256a6c8b01e5a0cc9bd676c0a621fa505a0f
leanfj <leanfj@hotmail.com>
2020-01-14 23:40:01 -0300

    Criado rota para salvar dev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,29 @@
 const { Router } = require('express');
-const axios = require('axios')
+const axios = require('axios');
+const developer = require('./models/developer');
 
 const routes = Router();
 
-routes.post('/developers', (req, res, next) => {
-    const { github_user } = req.body;
+routes.post('/developers', async (req, res, next) => {
+  const { github_username, techs } = req.body;
 
+  const response = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = response.data;
+
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const createResponse = await developer.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  return res.json(createResponse);
 });
-
 
 module.exports = routes;
